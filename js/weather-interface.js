b9365cab852fff5d353c2011ca67cd4929d5015c
Guest User <Guest@Epicodus-4F.local>
2017-06-26 16:18:41 -0700

    .env file for API key

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,11 +1,17 @@
-var apiKey = "2d4b9a9b29b1de9c7a87914a379bd372";
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#weather-location').click(function() {
     var city = $('#location').val();
     $('#location').val("");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
-    });
-  });
-});
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response) {
+         $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+       })
+
+      .fail(function(error) {
+         $('.showWeather').text(error.responseJSON.message);
+       });
+   });
+ });
