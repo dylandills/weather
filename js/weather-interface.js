4f438ff3b0f02633d75fb39ee2671c38e03edc74
Guest User <Guest@Epicodus-4F.local>
2017-06-26 16:02:23 -0700

    api functionality, utilizing bower and gulp asset pipeline

---
+++ js/weather-interface.js
@@ -0,0 +1,11 @@
+var apiKey = "2d4b9a9b29b1de9c7a87914a379bd372";
+
+$(document).ready(function() {
+  $('#weather-location').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey, function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    });
+  });
+});
